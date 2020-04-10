95b524436a6e811d8edb0a0a455bd44235f97cf0
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-10 17:29:41 -0500

    completed

--- DOM-array-methods/script.js
+++ DOM-array-methods/script.js
@@ -54,6 +54,14 @@ function showMillionaires() {
     updateDOM();
 }
 
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 //Add bew obj to data arr
 function addData(obj) {
     data.push(obj);
@@ -86,3 +94,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
