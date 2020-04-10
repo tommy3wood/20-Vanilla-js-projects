1bea2f6dd39960481ddf62c45051971eae65439b
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-10 13:35:12 -0500

    generate new users. regex for money

--- DOM-array-methods/script.js
+++ DOM-array-methods/script.js
@@ -31,4 +31,29 @@ async function getRandomUser() {
 //Add bew obj to data arr
 function addData(obj) {
     data.push(obj);
+
+    updateDOM();
 }
+
+//update DOM
+function updateDOM(providedData = data) {
+    //clear main div
+    main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+        main.appendChild(element);
+    });
+
+}
+
+//format number as money
+function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//event listeners
+
+addUserBtn.addEventListener('click', getRandomUser);
