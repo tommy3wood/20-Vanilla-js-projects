e815a61044bbdbdef41d4ea6a09a577c2b690ef8
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-14 14:21:23 -0500

    added modal transitions and eventlisteners

--- menu-slider-modal/script.js
+++ menu-slider-modal/script.js
@@ -7,3 +7,18 @@ const modal = document.getElementById('modal');
 toggle.addEventListener('click', () =>
     document.body.classList.toggle('show-nav')
 );
+
+//show modal
+open.addEventListener('click', () =>
+    modal.classList.add('show-modal')
+);
+
+// hide modal
+close.addEventListener('click', () =>
+    modal.classList.remove('show-modal')
+);
+
+//hide modal on outside click
+window.addEventListener('click', e =>
+    e.target == modal ? modal.classList.remove('show-modal') : false
+);
