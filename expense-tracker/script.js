6e99f4d7bc56ea53b83ad033852787305d7d23d4
Tom Curran <thomaswcurran1014@gmail.com>
2020-08-26 16:50:50 -0400

    Display balance, income, expense

--- expense-tracker/script.js
+++ expense-tracker/script.js
@@ -33,10 +33,36 @@ function addTransactionDOM(transaction) {
     list.appendChild(item);
 }
 
+//update the balance, income, expense
+function updateValues(){
+    const amounts = transactions.map(transaction =>
+        transaction.amount);
+
+    const total = amounts.reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+    const income = amounts
+                        .filter(item => item > 0)
+                        .reduce((acc, item) => (acc += item), 0)
+                        .toFixed(2);
+
+    const expense = (
+        amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => (acc += item), 0) 
+        * -1)
+        .toFixed(2);
+
+    balance.innerText = `$${total}`;
+    moneyPlus.innerText = `$${income}`;
+    moneyMinus.innerText = `$${expense}`;
+}
+
 //init app
 function init() {
     list.innerHTML = '';
     transactions.forEach(addTransactionDOM);
+    updateValues();
 }
 
 init();
